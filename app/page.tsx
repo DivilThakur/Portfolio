1dece46494fc227caf2b8bfc9885a525fed7c224
DivilThakur <divilthkr3@gmail.com>
2024-12-29 21:33:42 +0530

    csr fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import Clients from "@/components/Clients";
-import Experience from "@/components/Experience";
+
 import Footer from "@/components/Footer";
 import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
@@ -18,7 +18,7 @@ export default function Home() {
         <Grid/>
         <RecentProjects/>
         <Clients/>
-        <Experience/>
+      
         <Footer/>
       </div>
     </main>
